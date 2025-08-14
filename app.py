73cd851dc83f4f17874e3a71cc0be0ba647d6250
Jeong Woo Jin <jog50031013@gmail.com>
2025-08-15 02:19:15 +0900

    Fix: Correctly handle newlines in private_key

--- app.py
+++ app.py
@@ -57,7 +57,16 @@ def connect_to_sheet():
             "type": st.secrets["gcp_service_account"]["type"],
             "project_id": st.secrets["gcp_service_account"]["project_id"],
             "private_key_id": st.secrets["gcp_service_account"]["private_key_id"],
-            "private_key": st.secrets["gcp_service_account"]["private_key"].replace('\n', '\n'),
+            "private_key": st.secrets["gcp_service_account"]["private_key"].replace('\
+', '\n'),
+            "client_email": st.secrets["gcp_service_account"]["client_email"],
+            "client_id": st.secrets["gcp_service_account"]["client_id"],
+            "auth_uri": st.secrets["gcp_service_account"]["auth_uri"],
+            "token_uri": st.secrets["gcp_service_account"]["token_uri"],
+            "auth_provider_x509_cert_url": st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
+            "client_x509_cert_url": st.secrets["gcp_service_account"]["client_x509_cert_url"],
+            "universe_domain": "googleapis.com"
+        }
             "client_email": st.secrets["gcp_service_account"]["client_email"],
             "client_id": st.secrets["gcp_service_account"]["client_id"],
             "auth_uri": st.secrets["gcp_service_account"]["auth_uri"],
